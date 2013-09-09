cb75f6e332a1ef5151aeb895764bdebd4c02fd5a
Mark Roberts <wizzat@gmail.com>
2013-09-09 09:15:02 -0700

    Add test set_now, reset_now, and now

--- tests/testdateutil.py
+++ tests/testdateutil.py
@@ -1,15 +1,31 @@
 from pyutil.testutil import *
 from pyutil.dateutil import *
-import unittest, datetime
+import unittest, datetime, time
 
 class TestDateUtil(unittest.TestCase):
     def test_epoch_handling(self):
         self.assertEqual(from_epoch(1378741939), datetime.datetime(2013, 9, 9, 15, 52, 19))
         self.assertEqual(to_epoch(datetime.datetime(2013, 9, 9, 15, 52, 19)), 1378741939)
 
-    @skip_unfinished
     def test_now_can_be_set_and_reset(self):
-        pass
+        t1 = now()
+        time.sleep(0.1)
+        t2 = now()
+        self.assertNotEqual(t1, t2)
+
+        set_now(now())
+        t1 = now()
+        time.sleep(0.1)
+        t2 = now()
+        self.assertEqual(t1, t2)
+
+        t3 = now()
+        reset_now()
+        t1 = now()
+        time.sleep(0.1)
+        t2 = now()
+        self.assertEqual(t1, t2)
+        self.assertNotEqual(t1, t3)
 
     @skip_unfinished
     def test_coerce_date(self):
