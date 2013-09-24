394377686114163458ccd5aa72353afc736c5b57
Mark Roberts <wizzat@gmail.com>
2013-09-24 09:09:52 -0700

    Add pack/unpack tests, add compression exception tests

--- tests/testserialization.py
+++ tests/testserialization.py
@@ -38,6 +38,14 @@ class TestSerialization(unittest.TestCase):
         self.assertRaises(struct.error, lambda: write_int_set({ 0, 2**38 })) # Maximum serializable value is 2**38-1
         self.assertRaises(struct.error, lambda: write_int_set({ -1, 0, 1 })) # Can't serialize negatives
 
+    def test_int_set_compression_exceptions(self):
+        self.assertRaises(zlib.error, lambda: read_int_set(''.join([
+            '\x01\x00\x00\x00',                 # Num bitmasks
+            '\x0e\x00\x00\x00\x00\x00\x00\x00', # Bitmask, 0b1110
+            '\x01\x00\x00\x00',                 # Num Indexes
+            '\x00\x00\x00\x00'                  # First Index
+        ]), True))
+
     def test_int_set__randoms(self):
         random_list = [ random.randint(1, 2**38-1) for _ in xrange(100000) ]
 
@@ -46,28 +54,40 @@ class TestSerialization(unittest.TestCase):
             self.assertEqual(read_int_set(write_int_set(s, False), False), s)
             self.assertEqual(read_int_set(write_int_set(s, True), True), s)
 
-    @skip_unfinished
     def test_pack_iterable(self):
-        self.assertEqual(pack_iterable([ 1, 2, 3, 3], 'H'), '')
-        self.assertEqual(pack_iterable([ 1, 2048, 3, 3], 'H'), '')
+        self.assertEqual(pack_iterable([ 1, 2, 3, 3], 'H', False), ''.join([
+            '\x01\x00', # 1
+            '\x02\x00', # 2
+            '\x03\x00', # 3
+            '\x03\x00', # 3
+        ]))
 
-    @skip_unfinished
+        self.assertEqual(pack_iterable([ 1, 2, 3, 3], 'H', True), zlib.compress(''.join([
+            '\x01\x00', # 1
+            '\x02\x00', # 2
+            '\x03\x00', # 3
+            '\x03\x00', # 3
+        ])))
+
     def test_unpack_iterable(self):
-        self.assertEqual(unpack_iterable('', 'H'), [ 1, 2, 3, 3])
-        self.assertEqual(unpack_iterable('', 'H'), [ 1, 2048, 3, 3])
+        self.assertEqual(unpack_iterable(''.join([
+            '\x01\x00', # 1
+            '\x02\x00', # 2
+            '\x03\x00', # 3
+            '\x03\x00', # 3
+        ]), 'H', False), [ 1, 2, 3, 3 ])
 
-    @skip_unfinished
+        self.assertEqual(unpack_iterable(zlib.compress(''.join([
+            '\x01\x00', # 1
+            '\x02\x00', # 2
+            '\x03\x00', # 3
+            '\x03\x00', # 3
+        ])), 'H', True), [ 1, 2, 3, 3 ])
+
     def test_iterable__random(self):
-        random_list = [ random.randint(1, 2**64-1) for _ in xrange(25000) ]
+        random_list = [ random.randint(1, 2**64-1) for _ in xrange(100000) ]
 
         for _ in xrange(100):
-            s = [ random.choice(random_list) for _ in xrange(10000) ]
+            s = [ random.choice(random_list) for _ in xrange(100) ]
             self.assertEqual(unpack_iterable(pack_iterable(s, 'Q', False), 'Q', False), s)
             self.assertEqual(unpack_iterable(pack_iterable(s, 'Q', True), 'Q', True), s)
-
-    @skip_unfinished
-    def test_serialization(self):
-        s = { 1, 2, 3, 4, 2**30-1 }
-        for name, func_tpl in self.funcs.iteritems():
-            write_func, read_func = func_tpl
-            self.assertEqual(read_func(write_func(s)), s, name + str(s) + str(read_func(write_func(s))))
