d4318756a24464e5021c55bcbe5fbbc8fd87aacc
Mark Roberts <wizzat@gmail.com>
2013-09-24 09:02:32 -0700

    Flesh out tests for serialization

--- tests/testserialization.py
+++ tests/testserialization.py
@@ -1,25 +1,48 @@
-import unittest, random
+import unittest, random, zlib, struct
 from serialization import *
 from testutil import *
 
 class TestSerialization(unittest.TestCase):
-
-    @skip_unfinished
     def test_write_int_set(self):
-        self.assertEqual(write_int_set(set), '')
-        self.assertEqual(write_int_set({ 1, 2, 3 }, False), '')
-        self.assertEqual(write_int_set({ 1, 2, 3 }, False), '')
+        self.assertEqual(write_int_set(set()), '\x00\x00\x00\x00')
+        self.assertEqual(write_int_set({ 1, 2, 3 }, False), ''.join([
+            '\x01\x00\x00\x00',                 # Num bitmasks
+            '\x0e\x00\x00\x00\x00\x00\x00\x00', # Bitmask, 0b1110
+            '\x01\x00\x00\x00',                 # Num Indexes
+            '\x00\x00\x00\x00'                  # First Index
+        ]))
+        self.assertEqual(write_int_set({ 1, 2, 3 }, True), zlib.compress(''.join([
+            '\x01\x00\x00\x00',                 # Num bitmasks
+            '\x0e\x00\x00\x00\x00\x00\x00\x00', # Bitmask, 0b1110
+            '\x01\x00\x00\x00',                 # Num Indexes
+            '\x00\x00\x00\x00'                  # First Index
+        ])))
 
-    @skip_unfinished
     def test_read_int_set(self):
-        pass
+        self.assertEqual(read_int_set(''.join([
+            '\x01\x00\x00\x00',                 # Num bitmasks
+            '\x0e\x00\x00\x00\x00\x00\x00\x00', # Bitmask, 0b1110
+            '\x01\x00\x00\x00',                 # Num Indexes
+            '\x00\x00\x00\x00'                  # First Index
+        ]), False), { 1, 2, 3 })
 
-    @skip_unfinished
+        self.assertEqual(read_int_set(zlib.compress(''.join([
+            '\x01\x00\x00\x00',                 # Num bitmasks
+            '\x0e\x00\x00\x00\x00\x00\x00\x00', # Bitmask, 0b1110
+            '\x01\x00\x00\x00',                 # Num Indexes
+            '\x00\x00\x00\x00'                  # First Index
+        ])), True), { 1, 2, 3 })
+
+    def test_serialization_limits(self):
+        self.assert_(write_int_set({ 0, 2**38-1 }))
+        self.assertRaises(struct.error, lambda: write_int_set({ 0, 2**38 })) # Maximum serializable value is 2**38-1
+        self.assertRaises(struct.error, lambda: write_int_set({ -1, 0, 1 })) # Can't serialize negatives
+
     def test_int_set__randoms(self):
         random_list = [ random.randint(1, 2**38-1) for _ in xrange(100000) ]
 
-        for _ in xrange(1000):
-            s = { random.choice(random_list) for _ in xrange(10000) }
+        for _ in xrange(100):
+            s = { random.choice(random_list) for _ in xrange(100) }
             self.assertEqual(read_int_set(write_int_set(s, False), False), s)
             self.assertEqual(read_int_set(write_int_set(s, True), True), s)
 
