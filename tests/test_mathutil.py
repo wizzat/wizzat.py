83c5e49e7ec402a3d8a969fe86735e952369362e
Mark Roberts <wizzat@gmail.com>
2015-01-28 15:24:47 -0500

    Improve accuracy of Percentile and add RollingTimePercentile

--- tests/test_mathutil.py
+++ tests/test_mathutil.py
@@ -2,32 +2,38 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 from builtins import *
 
 import math
-from wizzat.testutil import *
-from wizzat.mathutil import *
+import wizzat.testutil
+from wizzat.mathutil import Percentile, RollingTimePercentile
+from wizzat.dateutil import *
 
-class AverageTest(TestCase):
-    pass
+class PercentileTest(wizzat.testutil.TestCase):
+    def percentile(self, values):
+        p = Percentile()
 
-class PercentileTest(TestCase):
+        for value in values:
+            p.add_value(value)
+
+        return p
+
     def test_percentile__100(self):
-        p = Percentile(*range(100))
+        p = self.percentile(range(100))
         self.assertEqual(p.percentile(0.0), 0)
-        self.assertEqual(p.percentile(.25), 23)
-        self.assertEqual(p.percentile(0.5), 48)
+        self.assertEqual(p.percentile(.25), 24)
+        self.assertEqual(p.percentile(0.5), 49)
         self.assertEqual(p.percentile(.75), 73)
-        self.assertEqual(p.percentile(1.0), 100)
+        self.assertEqual(p.percentile(1.0), 99)
 
     def test_percentile__1000(self):
-        p = Percentile(*range(1000))
+        p = self.percentile(range(1000))
         self.assertEqual(p.percentile(0.0), 0)
-        self.assertEqual(p.percentile(.25), 247)
-        self.assertEqual(p.percentile(0.5), 493)
-        self.assertEqual(p.percentile(.75), 748)
-        self.assertEqual(p.percentile(1.0), 1009)
+        self.assertEqual(p.percentile(.25), 248)
+        self.assertEqual(p.percentile(0.5), 494)
+        self.assertEqual(p.percentile(.75), 749)
+        self.assertEqual(p.percentile(1.0), 999)
 
     def test_percentile_deviation(self):
         r = range(0, 2**64, int(2**64/1000000))
-        p = Percentile(*(float(x) for x in r))
+        p = self.percentile(float(x) for x in r)
 
         for x in range(100):
             real_pct = r[int((x/100.0) * len(r))]
@@ -38,17 +44,65 @@ class PercentileTest(TestCase):
                 self.assertTrue(variance < 0.055, "{} != {}, {} {}".format(pct, real_pct, variance, x))
 
     def test_handles_zero(self):
-        p = Percentile(0, 0, 0, 1)
+        p = self.percentile([0, 0, 0, 1])
 
         self.assertEqual(p.percentile(0.0), 0.0)
         self.assertEqual(p.percentile(.98), 1.0)
         self.assertEqual(p.percentile(1.0), 1.0)
 
     def test_empty(self):
-        p = Percentile()
+        p = self.percentile([])
         self.assertEqual(p.percentile(0),   None)
         self.assertEqual(p.percentile(.25), None)
         self.assertEqual(p.percentile(.50), None)
         self.assertEqual(p.percentile(.75), None)
         self.assertEqual(p.percentile(.98), None)
         self.assertEqual(p.percentile(1.0), None)
+
+
+class RollingPercentileTest(PercentileTest):
+    def percentile(self, values):
+        p = RollingTimePercentile(10, 60)
+
+        for value in values:
+            p.add_value(value)
+
+        return p
+
+    def test_window_rolloff__empty(self):
+        set_now("2014-04-04 00:00:00")
+        p = self.percentile([])
+        p.add_value(1.0)
+        set_now("2014-04-04 01:00:01")
+
+        self.assertEqual(p.percentile(0),   None)
+        self.assertEqual(p.percentile(.25), None)
+        self.assertEqual(p.percentile(.50), None)
+        self.assertEqual(p.percentile(.75), None)
+        self.assertEqual(p.percentile(.98), None)
+        self.assertEqual(p.percentile(1.0), None)
+
+        self.assertEqual(p.max_value, None)
+        self.assertEqual(p.min_value, None)
+        self.assertEqual(p.num_values, None)
+        self.assertEqual(p.total, None)
+
+    def test_window_rolloff(self):
+        set_now("2014-04-04 00:00:00")
+        p = self.percentile([])
+        static_now = now()
+
+        for x in range(100):
+            p.add_value(x)
+            set_now(static_now + seconds(10*x))
+
+        self.assertEqual(p.percentile(0.0), 94)
+        self.assertEqual(p.percentile(.25), 94)
+        self.assertEqual(p.percentile(0.5), 96)
+        self.assertEqual(p.percentile(.75), 98)
+        self.assertEqual(p.percentile(1.0), 99)
+
+        self.assertEqual(p.max_value, 99)
+        self.assertEqual(p.min_value, 94)
+        self.assertEqual(p.num_values, 6)
+        self.assertEqual(p.total, 94+95+96+97+98+99)
--- wizzat/mathutil.py
+++ wizzat/mathutil.py
@@ -3,11 +3,15 @@ from builtins import *
 from future.utils import iteritems
 
 import collections
+import itertools
 import math
+
+from wizzat.dateutil import now, to_epoch
 
 __all__ = [
     'avg',
     'Percentile',
+    'RollingTimePercentile',
 ]
 
 def avg(iterable):
@@ -16,18 +20,102 @@ def avg(iterable):
     """
     return 1.0 * sum(iterable) / len(iterable)
 
-class Avg(object):
-    """
-    Calculates the running average.
 
-    a = Average()
+class RollingTimePercentile(object):
+    """
+    Logarithmic percentile approximation for non-negative values.
+    Expected variance from actual median is about 5%.
+    Expected memory consumption is ~8kb with constant time calculation.
+
+    p = Percentile()
     for x in iterable:
-        a.add_value(x)
+        p.add_value(x)
 
-    a.average()
+    p.percentile(0.0) # Min value
+    p.percentile(0.5) # Median
+    p.percentile(1.0) # Max value
     """
-    def __init__(self, *values):
-        pass
+    def __init__(self, window_sec, max_sec):
+        self.window_sec = window_sec
+        self.max_sec = max_sec
+        self.windows = {}
+
+    def add_value(self, value):
+        curr_window = to_epoch(now()) // self.window_sec
+        if curr_window not in self.windows:
+            self.windows[curr_window] = Percentile()
+            self.trim_windows()
+
+        self.windows[curr_window].add_value(value)
+
+    def trim_windows(self):
+        curr_window = to_epoch(now()) // self.window_sec
+        min_window = (to_epoch(now()) - self.max_sec) // self.window_sec
+
+        for key in list(self.windows.keys()):
+            if key < min_window:
+                self.windows.pop(key)
+
+    def percentile(self, pct):
+        if not 0.0 <= pct <= 1.0:
+            raise ValueError()
+
+        self.trim_windows()
+
+        if not self.windows:
+            return None
+
+        if pct == 0.0:
+            return min(x.min_value for x in self.windows.values())
+        elif pct == 1.0:
+            return max(x.max_value for x in self.windows.values())
+
+        num_values = 0
+        all_values = []
+        for window in self.windows.values():
+            num_values += window.num_values
+            all_values.extend(window.values.items())
+        all_values.sort(key=lambda x: x[0])
+
+        ct = 0
+        target = pct * num_values
+        for idx, idx_ct in all_values:
+            ct += idx_ct
+            if ct >= target:
+                break
+
+        def e(n):
+            return 10**(n/100.0)
+
+        return int(avg([e(idx), e(idx+.9)]))
+
+    def _window_value(self, op, default):
+        self.trim_windows()
+        if not self.windows:
+            return None
+
+        value = default
+        for window in self.windows.values():
+            value = op(value, window)
+
+        return value
+
+    @property
+    def num_values(self):
+        return self._window_value(lambda v, w: v + w.num_values, 0)
+
+    @property
+    def total(self):
+        return self._window_value(lambda v, w: v + w.total, 0)
+
+    @property
+    def min_value(self):
+        return self._window_value(lambda v, w: min(v, w.min_value), float("inf"))
+
+    @property
+    def max_value(self):
+        return self._window_value(lambda v, w: max(v, w.min_value), -1)
+
 
 class Percentile(object):
     """
@@ -47,6 +135,8 @@ class Percentile(object):
         self.values     = collections.defaultdict(int)
         self.total      = 0
         self.num_values = 0
+        self.max_value  = -1
+        self.min_value  = float("inf")
 
         for value in values:
             self.add_value(value)
@@ -56,33 +146,49 @@ class Percentile(object):
             return
 
         if value < 0:
-            raise ValueError()
+            raise ValueError(value)
 
-        idx = int(math.log10(value+1) * 100)
+        if value < self.min_value:
+            self.min_value = value
+
+        if value > self.max_value:
+            self.max_value = value
 
         self.total += value
-        self.values[idx] += 1
         self.num_values += 1
 
+        if value == 0:
+            value = 0.0000001
+
+        idx = int(math.log10(value) * 100)
+        self.values[idx] += 1
+
     def percentile(self, pct):
-        if pct > 1.0:
-            raise ValueError("pct > 1.0")
+        if not 0.0 <= pct <= 1.0:
+            raise ValueError()
 
         if not self.values:
             return None
+
+        if pct == 0.0:
+            return self.min_value
+        elif pct == 1.0:
+            return self.max_value
 
         target = pct * self.num_values
         ct = 0
 
         for idx, value in sorted(iteritems(self.values)):
-            ct += value 
+            ct += value
             if ct >= target:
                 break
 
         def e(n):
-            return 10**(n/100.0)-1
+            return 10**(n/100.0)
 
         return int(avg([e(idx), e(idx+.9)]))
+
+
 
     def __repr__(self):
         return "Percentile<min={},25={},50={},75={},98={},max={}>".format(
